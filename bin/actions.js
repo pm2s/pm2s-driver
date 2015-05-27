168a92d20b9089f23f57da5e2008868d16d0d9e0
s0ph1e <sonya1912@gmail.com>
2015-05-27 23:01:56 +0300

    Add pm2-axon, remove pm2

--- bin/actions.js
+++ bin/actions.js
@@ -1,18 +1,19 @@
-var pm2 = require('pm2');
+var axon  = require('pm2-axon');
+var sub = axon.socket('sub-emitter');
 
 function start() {
-	pm2.launchBus(function(err, bus) {
-		console.log('connected', bus);
-
-		bus.on('log:out', function(data) {
-			console.log(arguments);
-		});
+	// TODO: copy resolving pm2 socket path from https://github.com/Unitech/PM2/blob/master/constants.js
+	var socketPath = '/home/vagrant/.pm2/pub.sock';
+	sub.connect(socketPath); // [socket path] is '~/.pm2/pub.sock' by default.
+	sub.on('*', function(e, d){
+		console.log('[' + d.process.name + ']: ', e, d.event ? d.event : '');
+		if (d.event == 'exit') {
+			console.log(d);
+		}
 	});
 }
 
-function stop() {
-	pm2.disconnectBus();
-}
+function stop() {}
 
 module.exports = {
 	start: start,
